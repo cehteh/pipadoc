16e4fc4e55b0bb6d27d0a4831ddc57bdef18f056
Christian Thäter <ct@pipapo.org>
2020-08-22 02:29:26 +0200

    simplify <HEXSTRING> processing

--- pipadoc_config.lua
+++ pipadoc_config.lua
@@ -25,25 +25,23 @@ preprocessor_register (nil,
 
 
 --shipped_config_pre:
---: *  Replaces '<HEXSTRING>' in a opspec key with the first literal doublequoted string from the code
+--: *  Replaces '<HEXSTRING>' with the first literal doublequoted string from the code
 --:    converted to lowercase hexadecimal (useable for sorting including whitespace and punctuation).
 --:    When no literal doublequoted string exists in the code, then the last used hexadecimal
---:    string is used. Used for sorting documentation by the lifted string literal.
---TODO: remove the opspec requirement, just replace it always
+--:    string found is used. Used for sorting documentation by the lifted string literal.
 local hexstring
 
 preprocessor_register (nil,
                        function (context)
-                         --TODO: use COMMENTS_TABLE
-                         local literal_string = context.SOURCE:match('^[^"]*"([^"]*)".*%p*[%w][%w_]*%.<HEXSTRING>%p')
+                         local literal_string = context.SOURCE:match('^[^"]*"([^"]*)".-<HEXSTRING>')
 
                          if literal_string then
                            hexstring = string_tohex(literal_string:lower())
                          end
 
-                         if context.SOURCE:match('^.*%p*[%w][%w_]*%.<HEXSTRING>%p') then
-                           return context.SOURCE:gsub('^(.*%p*[%w][%w_]*%.)<HEXSTRING>(%p)',
-                                                      "%1"..hexstring.."%2",
+                         if context.SOURCE:match('<HEXSTRING>') then
+                           return context.SOURCE:gsub('<HEXSTRING>',
+                                                      hexstring,
                                                       1
                            )
                          end
